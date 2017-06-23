7d5ccda411fe6ecae4ebc8caa9ecfaa54927f528
Anton Kechashin <atk@sitecore.net>
2017-06-23 14:42:32 +0300

    Initial commit.

---
+++ src/Sitecore.Support.169753/Shell/Applications/ContentEditor/Checkbox.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+  using System;
+  public class Checkbox : Sitecore.Shell.Applications.ContentEditor.Checkbox
+  {
+    protected override void OnInit(EventArgs e)
+    {
+      #region Added code
+      this.Disabled = this.ReadOnly; 
+      #endregion
+      base.OnInit(e);
+    }
+  }
+}
